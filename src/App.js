8acc191a95955bb3ca05f3e189b46d84a2ec21af
Ananthesh Alse <anantheshalsear@gmail.com>
2020-08-12 23:51:01 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';  
 import Main from './components/MainComponent';
-// import SampleUi from './components/sampleUi';
 import { ConfigureStore } from './redux/configureStores';
 
 
@@ -13,7 +12,6 @@ function App() {
     <Provider store={store}> 
       <div className="App">
         <Main/>
-        {/* <SampleUi/> */}
       </div>
     </Provider>
   );
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,4 @@
 import React,{Component} from 'react';
-// import Modal from "react-modal";
 import  styled  from  'styled-components';
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
 import CKEditor from "@ckeditor/ckeditor5-react"
@@ -43,50 +42,30 @@ class Header extends Component {
             <div className="input-placeholder">
                 <span className="fa fa-search "></span> 
                 <input  type="text" placeholder="Search..." onChange={this.props.handleSerch}/>
-                {/* <input type="submit" value="search" onClick={(e)=>{this.setState({searchitem:""});}}  ></input>  */}
                 <span className="fa fa-times "></span>
-                {/* <input type="submit" value="Search"  /> */}
             </div>
             <div className="button-container" >
                 <Button onClick={this.toggleModal} >New Post</Button>
                 <Button onClick={this.props.posts} >Published</Button>
              </div>
-            {/* <button onClick={this.toggleModal}>Open modal</button> */}
-            {/* <Modal
-            isOpen={this.state.isOpen}
-            onRequestClose={this.toggleModal}
-            contentLabel="My dialog"
-            className="mymodal"
-            overlayClassName="myoverlay"
-            closeTimeoutMS={500}
-            >
-            <div className="leftsection">  
-                    Employee Name : <input onChange={this.props.handletitleChange
-                    } value={this.props.title} type="text" placeholder="Employee Name" /> <br />  
-                    Employee Department :  <input onChange={this.props.handletextChange} value={this.props.text} type="text" placeholder="Employee Department" /><br />  
-                    <button onClick={this.props.submitData}>ADD</button>   
-                    <button onClick={this.props.submitData}>CLEAR</button>  
-            </div>
-            <button onClick={this.toggleModal}>Close modal</button>
-            </Modal> */}
-            {this.state.isOpen ? 
-            <div className="leftsection">  
-                    <div className="inputfield"><input  value={this.props.title} type="text" placeholder="Title" onChange={this.props.handletitleChange} /></div>
-                    <div className="editer-container">
-                        <div className="editor">
-                            <CKEditor
-                            editor={ClassicEditor}
-                            data={this.state.isOpentext}
-                            onChange={(event, editor) => {
-                                const data = editor.getData()
-                                this.setText(data)
-                            }}
-                            />
-                        </div>
-                    </div>
-                    <Button onClick={() => { this.props.submitData();this.toggleModal();}} >ADD</Button>
-            </div> 
-        : null}
+              {this.state.isOpen ? 
+              <div className="leftsection">  
+                      <div className="inputfield"><input  value={this.props.title} type="text" placeholder="Title" onChange={this.props.handletitleChange} /></div>
+                      <div className="editer-container">
+                          <div className="editor">
+                              <CKEditor
+                              editor={ClassicEditor}
+                              data={this.state.isOpentext}
+                              onChange={(event, editor) => {
+                                  const data = editor.getData()
+                                  this.setText(data)
+                              }}
+                              />
+                          </div>
+                      </div>
+                      <Button onClick={() => { this.props.submitData();this.toggleModal();}} >ADD</Button>
+              </div> 
+          : null}
         </>
       
       );
